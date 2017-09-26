b72ae0c3454dcbbe3c14aaf25c8b93c3f18d86dc
alexeyche <alexey.chernushev@gmail.com>
2017-09-27 00:16:13 +0200

    junk

--- src/py-sparse-map/tf_model/fista.py
+++ src/py-sparse-map/tf_model/fista.py
@@ -16,7 +16,7 @@ input_size = 1
 seq_size = 500
 filter_len = 100
 layer_size = 50
-alpha = 0.1
+alpha = 2.0
 lrate = 1e-02 # Adam
 
 
@@ -34,6 +34,8 @@ for bi in xrange(batch_size):
 
 b = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, input_size), name="b")
 
+t = tf.placeholder(tf.float32, shape=(), name="t")
+
 D = tf.get_variable("D", [filter_len, 1, input_size, layer_size], 
     initializer=tf.random_normal_initializer()
 )
@@ -41,26 +43,41 @@ D = tf.get_variable("D", [filter_len, 1, input_size, layer_size],
 L = tf.reduce_sum(tf.square(D))
 
 x = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, layer_size), name="x")
+z = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, layer_size), name="z")
 
 b_hat = tf.nn.conv2d(
-	x,
+	z,
 	tf.transpose(D, (0, 1, 3, 2)), 
 	strides=[1, 1, 1, 1], 
 	padding='SAME',
 	name="b_hat"
 )
 
-error = b - b_hat
-
-x_grad = tf.nn.conv2d(
-	error, 
+z_grad = tf.nn.conv2d(
+	b - b_hat, 
 	D, 
 	strides=[1, 1, 1, 1], 
 	padding='SAME', 
 	name="z_grad"
 )
 
-new_x = shrink(x + 0.05*x_grad/L, alpha/L)   # x
+new_x = shrink(z + 0.5*z_grad/L, alpha/L)   # x
+
+new_t = (1.0 + tf.sqrt(1.0 + 4.0 * t ** 2)) / 2.0
+
+new_z = new_x + ((t - 1.0) / new_t) * (new_x - x)
+
+
+b_hat_ans = tf.nn.conv2d(
+	x,
+	tf.transpose(D, (0, 1, 3, 2)), 
+	strides=[1, 1, 1, 1], 
+	padding='SAME',
+	name="b_hat_ans"
+)
+
+error = tf.reduce_sum(tf.square(b - b_hat_ans))
+
 
 ###
 
@@ -68,26 +85,33 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 x_v = np.zeros(x.get_shape().as_list())
+z_v = x_v.copy()
 
+t_v = 1
 try:
-	for e in xrange(100):
+	for e in xrange(50):
 
-		b_hat_v, x_v, L_v, x_grad_v, error_v, D_v = sess.run(
+		b_hat_v, b_hat_ans_v, x_v, z_v, t_v, L_v, z_grad_v, error_v, D_v = sess.run(
 			[
 				b_hat,
+				b_hat_ans,
 				new_x,
+				new_z,
+				new_t,
 				L,
-				x_grad, 
+				z_grad, 
 				error,
 				D
 			],
 			{
 				b: b_v.transpose((1, 0, 2)).reshape((batch_size, seq_size, input_size, 1)),
 				x: x_v,
+				z: z_v,
+				t: t_v,
 			}
 		)
 
-		print "Epoch {}, loss {}, |h| {}".format(e, np.sum(np.square(error_v)), np.mean(x_v))
+		print "Epoch {}, loss {}, |h| {}".format(e, np.mean(error_v), np.mean(x_v))
 except KeyboardInterrupt:
 	pass
 
