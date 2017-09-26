aa203e731e17938f2383dea42cf183f17975ca13
alexeyche <alexey.chernushev@gmail.com>
2017-09-27 00:07:19 +0200

    junk

---
+++ src/py-sparse-map/tf_model/_ista.py
@@ -0,0 +1,125 @@
+
+
+import tensorflow as tf
+import numpy as np
+from util import *
+
+from tf_model.ts_pp import generate_ts
+
+
+np.random.seed(1)
+tf.set_random_seed(1)
+
+
+batch_size = 1
+input_size = 1
+seq_size = 500
+filter_len = 100
+layer_size = 50
+weight_init_factor = 0.05
+alpha = 0.2
+lam = 1.0
+lrate = 1e-02 # Adam
+# lrate = 5e-02 # Adadelta
+# lrate = 1e-04 # SGD
+
+x_v = np.zeros((seq_size, batch_size, input_size))
+
+
+for bi in xrange(batch_size):
+    for ni in xrange(input_size):
+        x_v[:,bi,ni] = generate_ts(seq_size)
+
+####
+
+
+x = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, input_size), name="Input")
+
+D = tf.get_variable("D", [filter_len, 1, input_size, layer_size], 
+    initializer=tf.uniform_unit_scaling_initializer(factor=weight_init_factor)
+)
+
+
+h_given = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, layer_size), name="Input")
+
+h = tf.nn.conv2d(
+	x, 
+	D, 
+	strides=[1, 1, 1, 1], 
+	padding='SAME', 
+	name="h"
+)
+
+for _ in xrange(3):
+	x_hat = tf.nn.conv2d(
+		h,
+		tf.transpose(D, (0, 1, 3, 2)), 
+		strides=[1, 1, 1, 1], 
+		padding='SAME',
+		name="x_hat"
+	)
+
+	error = x - x_hat
+
+
+	h_grad = tf.nn.conv2d(
+		error, 
+		D, 
+		strides=[1, 1, 1, 1], 
+		padding='SAME', 
+		name="h_grad"
+	)
+
+
+
+	# h = tf.nn.relu(tf.abs(h + alpha * h_grad) - lam * alpha) * tf.sign(h) 
+
+l1Sparsity = tf.reduce_mean(tf.reduce_sum(tf.abs(h), reduction_indices=[1, 2, 3]))
+
+rec_loss = tf.reduce_mean(tf.reduce_sum(tf.square(error), reduction_indices=[1, 2, 3]))
+loss = rec_loss/2.0 + lam * l1Sparsity
+
+D_grad = tf.gradients(loss, [D])[0]
+
+##
+
+# optimizer = tf.train.AdadeltaOptimizer(lrate)
+optimizer = tf.train.AdamOptimizer(lrate)
+# optimizer = tf.train.GradientDescentOptimizer(lrate)
+
+apply_grads_step = tf.group(
+    optimizer.apply_gradients([(D_grad, D)]),
+    # tf.nn.l2_normalize(D, 0)
+)
+
+
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+# D_before = sess.run(D)
+# sess.run(tf.assign(D, tf.nn.l2_normalize(D, 0)))
+# raise Exception()
+# h_v = np.random.randn(*h.get_shape().as_list())
+# h_v = np.zeros(h.get_shape().as_list())
+try:
+	for e in xrange(1000):
+
+		x_hat_v, h_v, error_v, h_v, D_v, D_grad_v, h_grad_v, _ = sess.run(
+			[
+				x_hat,
+				h, 
+				error, 
+				h, 
+				D, 
+				D_grad,
+				h_grad,
+				apply_grads_step,
+			],
+			{
+				x: x_v.transpose((1, 0, 2)).reshape((batch_size, seq_size, input_size, 1)),
+			}
+		)
+
+		print "Epoch {}, loss {}, |h| {}".format(e, np.mean(error_v), np.mean(h_v))
+except KeyboardInterrupt:
+	pass
--- src/py-sparse-map/tf_model/ista.py
+++ src/py-sparse-map/tf_model/ista.py
@@ -13,112 +13,106 @@ tf.set_random_seed(1)
 
 batch_size = 1
 input_size = 1
-seq_size = 1000
+seq_size = 500
 filter_len = 100
 layer_size = 50
-weight_init_factor = 0.2
-alpha = 0.5
-lam = 0.01
-lrate = 2e-03
+alpha = 1.0
+lrate = 1e-02 # Adam
 
-x_v = np.zeros((seq_size, batch_size, input_size))
 
-shrink = lambda a, b: tf.sign(a) * tf.maximum(tf.abs(a) - b, 0) 
+shrink = lambda a, b: tf.nn.relu(tf.abs(a) - b) * tf.sign(a)
+
+b_v = np.zeros((seq_size, batch_size, input_size))
+
 
 for bi in xrange(batch_size):
     for ni in xrange(input_size):
-        x_v[:,bi,ni] = generate_ts(seq_size)
+        b_v[:,bi,ni] = generate_ts(seq_size)
 
 ####
 
 
-x = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, input_size), name="Input")
+b = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, input_size), name="b")
+
+t = tf.placeholder(tf.float32, shape=(), name="t")
 
 D = tf.get_variable("D", [filter_len, 1, input_size, layer_size], 
-    initializer=tf.uniform_unit_scaling_initializer(factor=weight_init_factor)
+    initializer=tf.random_normal_initializer()
 )
 
+L = tf.reduce_sum(tf.square(D))
 
-h_given = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, layer_size), name="Input")
+x = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, layer_size), name="x")
+z = tf.placeholder(tf.float32, shape=(batch_size, seq_size, 1, layer_size), name="z")
 
-h = tf.nn.conv2d(
-	x, 
+b_hat = tf.nn.conv2d(
+	z,
+	tf.transpose(D, (0, 1, 3, 2)), 
+	strides=[1, 1, 1, 1], 
+	padding='SAME',
+	name="b_hat"
+)
+
+z_grad = tf.nn.conv2d(
+	b - b_hat, 
 	D, 
 	strides=[1, 1, 1, 1], 
 	padding='SAME', 
-	name="h"
+	name="z_grad"
 )
 
-for _ in xrange(5):
-	x_hat = tf.nn.conv2d(
-		h,
-		tf.transpose(D, (0, 1, 3, 2)), 
-		strides=[1, 1, 1, 1], 
-		padding='SAME',
-		name="x_hat"
-	)
+new_x = shrink(z + z_grad/L, alpha/L)   # x
 
-	error = x_hat - x
+new_t = (1.0 + tf.sqrt(1.0 + 4.0 * t ** 2)) / 2.0
+
+new_z = new_x + ((t - 1.0) / new_t) * (new_x - x)
 
 
-	h_grad = tf.nn.conv2d(
-		error, 
-		D, 
-		strides=[1, 1, 1, 1], 
-		padding='SAME', 
-		name="h_grad"
-	)
-
-
-
-	h = h - alpha * h_grad
-
-	h = shrink(h, alpha * lam)
-
-	# h = tf.Print(h, [tf.reduce_mean(h)])
-
-##
-
-loss = tf.nn.l2_loss(error)
-D_grad = tf.gradients(loss, [D])[0]
-
-##
-
-optimizer = tf.train.AdamOptimizer(lrate)
-
-apply_grads_step = tf.group(
-    optimizer.apply_gradients([(D_grad, D)]),
-    tf.nn.l2_normalize(D, 0)
+b_hat_ans = tf.nn.conv2d(
+	x,
+	tf.transpose(D, (0, 1, 3, 2)), 
+	strides=[1, 1, 1, 1], 
+	padding='SAME',
+	name="b_hat_ans"
 )
 
+error = tf.reduce_sum(tf.square(b - b_hat_ans))
 
+
+###
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-# D_before = sess.run(D)
-# sess.run(tf.assign(D, tf.nn.l2_normalize(D, 0)))
-# raise Exception()
-# h_v = np.random.randn(*h.get_shape().as_list())
-# h_v = np.zeros(h.get_shape().as_list())
+
+x_v = np.zeros(x.get_shape().as_list())
+z_v = x_v.copy()
+
+t_v = 1
 try:
 	for e in xrange(100):
 
-		x_hat_v, h_v, error_v, h_v, D_v, D_grad_v, h_grad_v, _ = sess.run(
+		b_hat_v, b_hat_ans_v, x_v, z_v, t_v, L_v, z_grad_v, error_v, D_v = sess.run(
 			[
-				x_hat,
-				h, 
-				error, 
-				h, 
-				D, 
-				D_grad,
-				h_grad,
-				apply_grads_step,
+				b_hat,
+				b_hat_ans,
+				new_x,
+				new_z,
+				new_t,
+				L,
+				z_grad, 
+				error,
+				D
 			],
 			{
-				x: x_v.transpose((1, 0, 2)).reshape((batch_size, seq_size, input_size, 1)),
+				b: b_v.transpose((1, 0, 2)).reshape((batch_size, seq_size, input_size, 1)),
+				x: x_v,
+				z: z_v,
+				t: t_v,
 			}
 		)
 
-		print "Epoch {}, loss {}, |h| {}".format(e, np.mean(error_v), np.mean(h_v))
+		print "Epoch {}, loss {}, |h| {}".format(e, np.mean(error_v), np.mean(x_v))
 except KeyboardInterrupt:
 	pass
+
+
