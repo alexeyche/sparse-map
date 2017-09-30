1273d5b97bcfea565e390df089bc2731bfe32338
alexeyche <alexey.chernushev@gmail.com>
2017-09-30 18:06:35 +0200

    junk

--- src/py-sparse-map/spca/con_grad_u.py
+++ src/py-sparse-map/spca/con_grad_u.py
@@ -17,14 +17,16 @@ def l0_nonlin(v, t, p):
 
 data = load_iris()
 x_v = data.data
+eigvec = np.linalg.eig(np.cov(x_v.T))[1]
+PC = np.dot(x_v, eigvec)[:,0:2]
 
 np.random.seed(10)
 tf.set_random_seed(10)
 
 batch_size, input_size = x_v.shape
 dim_size = 2
-p = 0.25
-
+p = 0.25 
+# p = 0.1 # for exp nonlin
 
 A = tf.placeholder(tf.float32, shape=(input_size, input_size), name="A")
 r = tf.Variable(np.random.randn(input_size), dtype=tf.float32)
@@ -33,18 +35,15 @@ new_r = tf.matmul(tf.expand_dims(r, 0), A)
 
 new_r = tf.squeeze(new_r)
 
+# new_r = exp_nonlin(r, new_r, p)
 new_r = l0_nonlin(r, new_r, p)
 
 new_r = tf.nn.l2_normalize(new_r, 0)
+new_r = tf.assign(r, new_r)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-Av = np.cov(x_v.T)
-
-eigvec = np.linalg.eig(Av)[1]
-
-PCA = np.dot(x_v, eigvec)[:,0:2]
 
 def find_pc(pi, epochs, x_v):
 	sess.run(tf.global_variables_initializer())
@@ -53,7 +52,7 @@ def find_pc(pi, epochs, x_v):
 
 	for e in xrange(epochs):
 		rv = sess.run(new_r, {A: Av})
-		if e % 5 == 0:
+		if e % 1 == 0:
 			print "Epoch {}, {}".format(e, np.sum(np.abs(rv) - np.abs(eigvec[:,pi])))
 	return rv
 
@@ -62,19 +61,19 @@ def find_pc(pi, epochs, x_v):
 pc0 = find_pc(0, 20, x_v)
 pc1 = find_pc(1, 20, x_v - np.outer(pc0, np.dot(x_v, pc0)).T)
 
-PCA_est = np.dot(x_v, np.asarray([pc0, pc1]).T)
+PC_est = np.dot(x_v, np.asarray([pc0, pc1]).T)
 
 shs(
-	PCA_est[np.where(data.target == 0)],
-	PCA_est[np.where(data.target == 1)],
-	PCA_est[np.where(data.target == 2)],
+	PC_est[np.where(data.target == 0)],
+	PC_est[np.where(data.target == 1)],
+	PC_est[np.where(data.target == 2)],
 	labels=["red", "blue", "green"],
 	show=False
 )
 
 shs(
-	PCA[np.where(data.target == 0)],
-	PCA[np.where(data.target == 1)],
-	PCA[np.where(data.target == 2)],
+	PC[np.where(data.target == 0)],
+	PC[np.where(data.target == 1)],
+	PC[np.where(data.target == 2)],
 	labels=["red", "blue", "green"],
 )
