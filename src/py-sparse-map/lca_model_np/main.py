d1fa26a01a62f953c9863cb31c761e8c622a5e31
Alexey Chernyshev <aleksei@spotify.com>
2017-10-12 22:17:48 +0200

    junk

--- src/py-sparse-map/lca_model_np/main.py
+++ src/py-sparse-map/lca_model_np/main.py
@@ -94,14 +94,14 @@ class LCALayer(object):
 
 
     def __call__(self, x, ti):
-        left_ti = max(0, ti-filter_len)
-
-        self.x_win[:, (filter_len-ti+left_ti):filter_len, :] = np.transpose(x[left_ti:ti], (1, 0, 2))
+        left_ti = max(0, ti-self.filter_len)
+        
+        self.x_win[:, (self.filter_len-ti+left_ti):self.filter_len, :] = np.transpose(x[left_ti:ti], (1, 0, 2))
         
         x_flat = self.x_win.reshape(self.batch_size, self.filter_len * self.input_size)
 
         if self.c.adaptive_threshold:
-            threshold = 10.0*np.mean(self.a_m)
+            threshold = 5.0*np.mean(self.a_m)
         else:
             threshold = self.c.lam
 
@@ -131,29 +131,29 @@ class LCALayer(object):
         self.a_m_seq[ti] = self.a_m
 
 
-    def learn(self):
+    def learn(self, sparse=False):
         if np.linalg.norm(self.dFc) > 1000.0:
             raise Exception(str(np.linalg.norm(self.dFc)))
-
+        
         self.F, self.Fc = self.c.opt.update((self.F, -self.dF), (self.Fc, -self.dFc))
-        
-        self.F = normf(self.F)
-        
-        # Fc = normf(Fc)
-        # self.Fc = np.minimum(self.Fc, 0.5)
-        self.Fc = np.dot(self.F.T, self.F) - np.eye(self.layer_size)
+
+        if not sparse:
+            self.F = normf(self.F)
+            
+            # Fc = normf(Fc)
+            # self.Fc = np.minimum(self.Fc, 0.5)
+            self.Fc = np.dot(self.F.T, self.F) - np.eye(self.layer_size)
         
 
 epochs = 10
 
-np.random.seed(5)
+np.random.seed(1)
 
 input_size = 1
 seq_size = 1000
 batch_size = 1
 layer_size = 100
-filter_len = 25
-
+filter_len = 24
 
 act = relu
 
@@ -165,10 +165,12 @@ for bi in xrange(batch_size):
         x[:,bi,ni] = np.diff(generate_ts(seq_size+1))
 
 # x[:,0,0] = 1.0*np.sin(np.linspace(0, 250, seq_size)/10.0)
+net = [
+    LCALayer(batch_size, filter_len, input_size, layer_size),
+    LCALayer(batch_size, filter_len/2, layer_size, layer_size/2),
+]
 
-layer = LCALayer(batch_size, filter_len, input_size, layer_size)
-
-layer.init_config(
+net[0].init_config(
     adaptive_threshold=True,
     lam=0.005,
     opt=SGDOpt((1.0, 1.0)),
@@ -176,38 +178,52 @@ layer.init_config(
     # opt=AdamOpt((0.005, 0.005), beta1=0.9, beta2=0.999, eps=1e-05),
 )
 
+net[0].init_config(
+    adaptive_threshold=True,
+    lam=0.005,
+    opt=SGDOpt((5.0, 5.0)),
+    # opt=MomentumOpt((0.05, 0.05), 0.9)
+    # opt=AdamOpt((0.005, 0.005), beta1=0.9, beta2=0.999, eps=1e-05),
+)
 
 # opt = AdamOpt((0.01, 0.01), beta1=0.9, beta2=0.999, eps=1e-05)
 # opt = NesterovMomentumOpt((0.1, 0.1), 0.99)
 # opt = SGDOpt((5.0, 5.0))
 
 
-a_hist = []
 try:
-    for e in xrange(1000):
-        layer.init(seq_size)
+    for e in xrange(100):
+        [l.init(seq_size) for l in net]
 
         for ti in xrange(seq_size):
-            layer(x, ti)
+            layer_input = x
+            for layer in net:
+                layer(layer_input, ti)
+                layer_input = layer.a_seq
 
-        layer.learn()
-
-        error_profile = np.sum(np.square(layer.x_hat-x), 2)
-        error = np.sum(error_profile)
+        [l.learn() for l in net]
         
-        if e % 10 == 0:
-            a_hist.append(layer.a_seq.copy())
-        print "Epoch {}, err_acc {:.4f}, MSE {:.8f}".format(
+        layer_input = x
+        error_profile = []
+        for l in net:
+            error_profile.append(np.sum(np.square(l.x_hat-layer_input), 2))
+            layer_input = l.a_seq
+        
+        error = [np.sum(ep) for ep in error_profile]
+        
+        print "Epoch {}, err_acc {}, MSE {}".format(
             e, 
-            layer.err_acc,
-            error,
+            ", ".join(["{:.4f}".format(l.err_acc) for l in net]),
+            ", ".join(["{:.8f}".format(mse) for mse in error]),
         )
 except KeyboardInterrupt:
     pass
 
-a_hist = np.asarray(a_hist)
+# a_hist = np.asarray(a_hist)
 
 # shm(a_seq, show=False)
 # shl(a_m_seq)
-shl(x, layer.x_hat, show=True)
+shl(x, layer0.x_hat, show=False)
+shm(layer0.a_seq, layer1.x_hat, show=True)
+
 # shm(F-F_init)
