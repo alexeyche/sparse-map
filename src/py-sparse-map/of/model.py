6ded23b3b51c0b1df141c8f2a2b17edca17071c1
alexeyche <alexey.chernushev@gmail.com>
2017-10-24 08:16:00 +0200

    junk

--- src/py-sparse-map/of/model.py
+++ src/py-sparse-map/of/model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 class SparseLayer(object):
-	def __init__(self, batch_size, input_size, layer_size, h, adapt_gain, tau_m):
+	def __init__(self, batch_size, input_size, layer_size, tau, h, adapt_gain, tau_m, feedback_const, ):
 		self.batch_size = batch_size
 		self.input_size = input_size
 		self.layer_size = layer_size
@@ -18,7 +18,8 @@ class SparseLayer(object):
 		self.h = h
 		self.adapt_gain = adapt_gain
 		self.tau_m = tau_m
-
+		self.feedback_const = feedback_const
+		self.tau = tau
 	@property
 	def init_state(self):
 		return tf.zeros(dtype=tf.float32, shape=(self.batch_size, self.layer_size)), tf.zeros(dtype=tf.float32, shape=(self.batch_size, self.layer_size))
@@ -34,14 +35,21 @@ class SparseLayer(object):
 		
 		gain = tf.matmul(r, self.D)
 
-		new_u = u + self.h * (-u + gain)/5.0
+		new_u = u + self.h * (-u + gain)/self.tau
 
-		return (new_u, tf.nn.relu(new_u - self.a_m)), se
+		return (new_u, tf.nn.relu(new_u - self.a_m)), se, r
 
 	def final_state(self, state):
 		u, a = state
 
 		return u, a, (self.adapt_gain*tf.reduce_mean(a, 0) - self.a_m)/self.tau_m
+
+	def feedback(self, state, top_down_signal):
+		u, a = state
+
+		return u + self.feedback_const * top_down_signal, a
+
+
 
 class AELayer(object):
 	def __init__(self, batch_size, input_size, layer_size):
--- src/py-sparse-map/of/simple_sparse_test.py
+++ src/py-sparse-map/of/simple_sparse_test.py
@@ -7,16 +7,15 @@ import os
 from of.model import SparseLayer
 
 input_size = 28*28
-layer_size = 144
+# layer_size = 144
+layer_size = 1000
 batch_size = 100
 lam = 0.1
 threshold = 0.1
-h = 0.1
 lrate = 1e-03
-tau_m = 1000.0
-adapt_gain = 100.0
 
 np.random.seed(42)
+tf.set_random_seed(42)
 
 S = lambda x: tf.log(1.0 + tf.square(x))
 dS = lambda x: 2.0 * x / (tf.square(x) + 1.0)
@@ -26,7 +25,13 @@ rs = lambda x: x.reshape(28, 28)
 	
 ################
 
-
+config = dict(
+	tau = 5.0,
+	tau_m = 1000.0,
+	adapt_gain = 100.0,
+	feedback_const = -0.1,
+	h = 0.1,
+)
 
 
 I = tf.placeholder(tf.float32, shape=(None, input_size), name="I")
@@ -37,19 +42,30 @@ batch_size = tf.shape(I)[0]
 layers_num = 3
 
 l0, l1, l2 = (
-	SparseLayer(batch_size, input_size, layer_size, h, adapt_gain, tau_m),
-	SparseLayer(batch_size, layer_size, layer_size/2, h, adapt_gain, tau_m),
-	SparseLayer(batch_size, layer_size/2, 2, h, adapt_gain, tau_m),
+	SparseLayer(batch_size, input_size, layer_size, **config),
+	SparseLayer(batch_size, layer_size, layer_size/2, **config),
+	SparseLayer(batch_size, layer_size/2, 2, **config),
 )
 
 l0s = l0.init_state
 l1s = l1.init_state
 l2s = l2.init_state
 
+errors = []
+
 for _ in xrange(10):	
-	l0s, se0 = l0(l0s, I)
-	l1s, se1 = l1(l1s, l0s[-1])
-	l2s, se2 = l2(l2s, l1s[-1])
+	l0s, se0, resid0 = l0(l0s, I)
+	l1s, se1, resid1 = l1(l1s, l0s[-1])
+	l2s, se2, resid2 = l2(l2s, l1s[-1])
+
+	# se0 = tf.Print(se0, [se0], message="0l: ")
+	# se1 = tf.Print(se1, [se1], message="1l: ")
+	# se2 = tf.Print(se2, [se2], message="2l: ")
+	
+	# errors.append((se0, se1, se2))
+	
+	l1s = l1.feedback(l1s, resid2)
+	l0s = l0.feedback(l0s, resid1)
 
 
 final_state0 = l0.final_state(l0s)
@@ -81,7 +97,7 @@ sess.run(tf.global_variables_initializer())
 a_m_v = [np.zeros(l.a_m.get_shape().as_list()) for l in (l0, l1, l2)]
 
 train_batch_size = 100
-test_batch_size = 1000
+test_batch_size = 5000
 num_batches = mnist.train.num_examples/train_batch_size
 
 def test(epoch):
@@ -100,8 +116,7 @@ def test(epoch):
 	shs(fs_v[-1][1], labels=(y_v,), file="{}/tmp/mnist_{}.png".format(os.environ["HOME"], epoch), figsize=(25,10))
 
 try:
-	
-	for e in xrange(1):
+	for e in xrange(100):
 		se_acc = np.zeros(layers_num)
 		for bi in xrange(num_batches):
 			x_v, y_v = mnist.train.next_batch(train_batch_size) 
@@ -125,7 +140,7 @@ try:
 			a_m_v = tuple(fsv[-1] for fsv in final_state_v)
 
 			se_acc += np.asarray(se_v)/num_batches
-		
+
 		if e % 5 == 0:
 			test(e)
 
